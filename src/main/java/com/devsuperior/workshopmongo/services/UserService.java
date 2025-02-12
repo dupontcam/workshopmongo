94142850aa75b7abdafc6a41e131dfdec288ccfe
dupontcam <125046483+dupontcam@users.noreply.github.com>
2025-02-12 08:37:35 -0300

    GET user by id

--- src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -1,14 +1,15 @@
 package com.devsuperior.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.devsuperior.workshopmongo.models.dto.UserDTO;
 import com.devsuperior.workshopmongo.models.entities.User;
 import com.devsuperior.workshopmongo.repositories.UserRepository;
+import com.devsuperior.workshopmongo.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,4 +22,9 @@ public class UserService {
 		return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 	}
 
+	public UserDTO findById(String id) {
+		Optional<User> result = repository.findById(id);
+		User entity = result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
+		return new UserDTO(entity);		
+	}
 }
